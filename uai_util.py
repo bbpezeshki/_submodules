bf20d1992b4237648dd2ddca7ed0754f9263781b
Bobak Pezeshki <bbpezeshki@gmail.com>
2023-01-24 15:44:10 -0800

    added uai model reader

---
+++ uai_util.py
@@ -0,0 +1,84 @@
+import io
+from pathlib import Path
+
+def initiateInputStream(f):
+    fin = None;
+    needToCloseFile = False;
+    if isinstance(f, io.TextIOBase):
+        fin = f;
+    else:
+        p = Path(f)
+        fin = p.open('r')
+    return fin, needToCloseFile
+
+def fullTableSizeFromDomains(f, uaiModel):
+    ntuples = 1;
+    for v in uaiModel["scopes"][f]:
+        ntuples *= uaiModel["domainSizes"][v]
+    return ntuples;
+
+def readUaiModel(f):
+    uaiModel = {
+                    "type"          :   None,
+                    "nvars"         :   None,
+                    "domainSizes"   :   None,
+                    "nfxns"         :   None,
+                    "scopes"        :   None,
+                    "fxntbls"       :   None,
+               }
+
+    fin, needToCloseFile = initiateInputStream(f)
+
+    tokens = []
+    for line in fin:
+        sline = line.strip()
+        if not line or not line[0].isalnum():
+            continue;
+        tokens += sline.split()
+    if needToCloseFile:
+        fin.close()
+
+    idx = 0
+    
+    uaiModel["type"] = tokens[idx]
+    idx +=1;
+
+    uaiModel["nvars"] = int(tokens[idx])
+    idx +=1;
+
+    domainSizes = [None]*uaiModel["nvars"]
+    for i in range(uaiModel["nvars"]):
+        domainSizes[i] = int(tokens[idx])
+        idx += 1
+    uaiModel["domainSizes"] = domainSizes
+
+    uaiModel["nfxns"] = int(tokens[idx])
+    idx += 1;
+
+    scopes = [None] * uaiModel["nfxns"]
+    for f in range(uaiModel["nfxns"]):
+        scope = None;
+        scopeSize = int(tokens[idx])
+        idx += 1
+        scope = [None]*scopeSize
+        for s in range(scopeSize):
+            scope[s] = int(tokens[idx])
+            idx += 1
+        scopes[f] = scope
+    uaiModel["scopes"] = scopes
+
+    fxntbls = [None] * uaiModel["nfxns"]
+    for f in range(uaiModel["nfxns"]):
+        ntuples = int(tokens[idx])
+        assert(ntuples==fullTableSizeFromDomains(f,uaiModel))
+        idx += 1
+        table = [None]*ntuples
+        for t in range(ntuples):
+            table[t] = float(tokens[idx])
+            idx += 1
+        fxntbls[f] = table
+    uaiModel["fxntbls"] = fxntbls
+
+    assert(idx==len(tokens))
+
+    return uaiModel
