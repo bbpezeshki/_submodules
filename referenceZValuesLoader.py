45f25aa52b1ef876f2dc52d856551e8fd4308b71
Bobak Pezeshki <bbpezeshki@gmail.com>
2024-01-28 13:59:49 -0800

    automate refZ loading with size argument

--- referenceZValuesLoader.py
+++ referenceZValuesLoader.py
@@ -1,7 +1,18 @@
 from pathlib import Path
 import csv
 
-def loadReferenceZValues(refZValCSVFile = "/home/bbpezeshki/VSCode/haplo1/ProblemDomains/TaskBased/Z/exactZvalues.csv"):
+def loadReferenceZValues(refZValCSVFile=None, size=None):
+    # for version compatibility
+    if refZValCSVFile == None:
+        if size == None or size.upper() == "SMALL":
+            refZValCSVFile = "/home/bbpezeshki/VSCode/haplo1/ProblemDomains/TaskBased/Z/exactZvalues.csv"
+        elif size.upper() == "LARGE":
+            refZValCSVFile = "/home/bbpezeshki/VSCode/haplo1/ProblemDomains/TaskBased/Z/estimatedZvalues_byAverage.csv"
+        else:
+            assert(False)
+    else:
+        assert(size==None)
+
     refZValCSVFile = Path(refZValCSVFile)
     refZVals = {}
     with refZValCSVFile.open('r') as fin:
